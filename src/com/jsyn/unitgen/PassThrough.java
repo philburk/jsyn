55bfbe5a4c6f2c89ecbb8c21bcad6b90f260189a
Phil Burk <philburk@mobileer.com>
2017-03-26 09:55:06 -0700

    MultiPassThrough: multi-channel passthrough

--- src/com/jsyn/unitgen/PassThrough.java
+++ src/com/jsyn/unitgen/PassThrough.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +19,10 @@ package com.jsyn.unitgen;
 /**
  * Pass the input through to the output unchanged. This is often used for distributing a signal to
  * multiple ports inside a circuit. It can also be used as a summing node, in other words, a mixer.
- * 
+ *
  * @author Phil Burk (C) 2011 Mobileer Inc
  * @see Circuit
+ * @see MultiPassThrough
  */
 public class PassThrough extends UnitFilter {
     @Override
